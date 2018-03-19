3ef8ce4e3e16c433df7b44c8a499e5bcae386661
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-19 23:04:59 +0600

    Add files via upload

---
+++ Quotent of two numbers.py
@@ -0,0 +1,7 @@
+#program name: Quotent of two numbers
+#author name : Al- Ekram Hossain Abir
+#a program to get quotent of two numbers
+a=int(input())
+b=int(input())
+c= a/b
+print("Quotent of two number is: ",c)
