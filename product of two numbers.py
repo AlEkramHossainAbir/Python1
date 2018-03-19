3c3e744a38865691408c8a2e51ef20f10d86307f
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-19 22:54:24 +0600

    Add files via upload

---
+++ product of two numbers.py
@@ -0,0 +1,7 @@
+#program name: products of two number
+#author name: Al- Ekram Hossain Abir
+#a program to product of two number
+a=int(input())
+b=int(input())
+c=a*b
+print("product of two number is: ",c)
