3d63f366d20e9a4818443c40b4813d67b6f82b8f
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-19 22:10:13 +0600

    Add files via upload

---
+++ difference of two number.py
@@ -0,0 +1,8 @@
+#program name: difference of two number
+# author name : Al - Ekram Hossain Abir
+#a program to find difference between two number
+
+a=int(input())
+b=int(input())
+c=a-b
+print("the difference between two number is: ",c)
