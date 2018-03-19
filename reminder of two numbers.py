32a247a7a11a612e2bb987047a8e64d61844eb09
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-19 23:15:57 +0600

    Add files via upload

---
+++ reminder of two numbers.py
@@ -0,0 +1,8 @@
+#program name: reminder of two numbers
+#author name: Al- Ekram hossain Abir
+#a program to find reminder
+
+a=int(input())
+b=int(input())
+c=a%b
+print("reminder of two number is: ",c)
