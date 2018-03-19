887a15a9c6e32f36967ac588ba92fb942a509bee
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-19 22:04:49 +0600

    Add files via upload

---
+++ summation of two number.py
@@ -0,0 +1,8 @@
+#program name: summation of two number
+#author name: Al- Ekram Hossain Abir
+#a program to do sum of two numbers
+
+a=int(input())
+b=int(input())
+c=a+b
+print("the summation of two number is: ",c)
