a6b811f0460073510a5c7d260bdff95573dd1967
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-20 09:52:47 +0600

    Add files via upload

---
+++ pass or fail.py
@@ -0,0 +1,13 @@
+#program name: pass or fail
+#Author name: Al - Ekram Hossain Abir
+#a program to find pass or fail
+
+a=float(input())
+if(a<60):
+    {
+        print("Fail")
+    }
+else:
+    {
+        print("Pass")
+    }
