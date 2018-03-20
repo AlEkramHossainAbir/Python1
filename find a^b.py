0ca54536f0049344d9379c6cedbad47dc2d8ffb6
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-20 09:33:17 +0600

    Add files via upload

---
+++ find a^b.py
@@ -0,0 +1,18 @@
+#program name : find a^b
+#author name: Al- Ekram Hossain Abir
+#a program to get power
+#without using function:
+"""
+a=int(input())
+b=int(input())
+c=a**b
+print("The result is: ",c)
+"""
+
+#by function:
+
+a=int(input())
+b=int(input())
+c=pow(a,b)
+print("The result is: ",c)
+
