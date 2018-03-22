b7b6e8da48f8db857610b5dc4e35f058cdcfdef8
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-22 21:29:22 +0600

    Add files via upload

---
+++ Voter or non voter.py
@@ -0,0 +1,8 @@
+#program name: Voter or Non- Voter
+#author name : Al- Ekram Hossain Abir
+#a program to find voter or  non - voter
+age= int(input())
+if age>=18:
+    print("Voter")
+else:
+    print("Non- Voter")
