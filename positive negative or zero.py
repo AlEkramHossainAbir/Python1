ffb2cc4b14409ac1067512d2d14da80202df71c8
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-22 22:24:38 +0600

    Add files via upload

---
+++ positive negative or zero.py
@@ -0,0 +1,11 @@
+#program name: a number positive , negative or zero
+#author name : Al - Ekram Hossain Abir
+# a program to find a number positive , negative or zero
+
+a=int(input())
+if a>0:
+    print("Positive")
+elif a<0:
+    print("Negative")
+else:
+    print("Zero")
