d7015935baf0e9174e3d847192c6760619136824
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-22 23:04:18 +0600

    Add files via upload

---
+++ Maximum number.py
@@ -0,0 +1,10 @@
+#program nAme: Finding maximum number between two numbers
+#author name: Al- Ekram Hossain Abir
+# a program to find maximum
+
+a=int(input())
+b=int(input())
+if a>b:
+    print(a,"is maximum")
+else:
+    print(b,"is maximum")
