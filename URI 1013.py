7845eb0dc3a53fd42f11b14836c062e4bb5da6c9
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-24 13:00:28 +0600

    Add files via upload

---
+++ URI 1013.py
@@ -0,0 +1,11 @@
+#program name:URI 1013 (The Greatest)
+#author name: Al- Ekram Hossain Abir
+#a program to find greatest
+
+a=input()
+b,c,d=a.split(" ")
+b=int(b)
+c=int(c)
+d=int(d)
+x=max(b,c,d)
+print(x,"eh o maior")
