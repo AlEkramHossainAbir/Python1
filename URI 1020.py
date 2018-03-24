5c7a44382b73709faee50a58bf8381e42e4b20f3
Al- Ekram Hossain Abir <36513697+AlEkramHossainAbir@users.noreply.github.com>
2018-03-24 13:01:20 +0600

    Add files via upload

---
+++ URI 1020.py
@@ -0,0 +1,14 @@
+#program name: URI 1020 Age in Days
+#author name: Al- Ekram Hossain Abir
+
+a=int(input())
+b=a//365
+d=a%365
+m=d//30
+d=d%30
+#print(b,"ano(s)",m,"mes(es)",d,"dia(s)",end=" ")
+print(b,"ano(s)")
+print(m,"mes(es)")
+print(d,"dia(s)")
+
+
